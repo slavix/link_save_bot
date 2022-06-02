275b534c7ef0c658912d8b2962ad14ad5325db2d
Viacheslav Skoropupov <vs@keyba.ru>
2022-06-02 18:07:10 +0700

    telegram commands

---
+++ events/telegram/messages.go
@@ -0,0 +1,12 @@
+package telegram
+
+const msgHelp = `help message`
+
+const msgHello = " He there! \n\n" + msgHelp
+
+const (
+	msgUnknownCommand = "Unknown command"
+	msgNoSavedPages   = "You have no saved pages"
+	msgSaved          = "Saved!"
+	msgAlreadyExists  = "You have already have this page in your list"
+)
