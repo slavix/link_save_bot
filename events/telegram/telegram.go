2ce73c65567e429ef52ac8f32e9e9ac427bac760
Viacheslav Skoropupov <vs@keyba.ru>
2022-06-03 21:14:18 +0700

    processing

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -52,7 +52,7 @@ func (p *Processor) Fetch(limit int) ([]events.Event, error) {
 func (p *Processor) Process(event events.Event) error {
 	switch event.Type {
 	case events.Message:
-		p.processMessage(event)
+		return p.processMessage(event)
 	default:
 		return e.Wrap("can't process message", ErrUnknownEventType)
 	}
@@ -64,6 +64,12 @@ func (p *Processor) processMessage(event events.Event) error {
 	if err != nil {
 		return e.Wrap("can't process message", err)
 	}
+
+	if err = p.doCmd(event.Text, meta.ChatID, meta.Username); err != nil {
+		return e.Wrap("can't process message", err)
+	}
+
+	return nil
 }
 
 func meta(event events.Event) (Meta, error) {
